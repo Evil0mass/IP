2830648a10d9793978be28976bcfad031bb8f8a0
Evil0mass <2034529786@qq.com>
2017-06-13 09:20:57 +0000

    Hostname

--- update-ip.sh
+++ update-ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/IP
-ifconfig | grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}' > /root/IP/Hostname
+ifconfig | grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}' > /root/IP/Hostname.md
 git add .
 git commit -am "Hostname"
 git push
